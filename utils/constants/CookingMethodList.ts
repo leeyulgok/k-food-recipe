12e60677f33be47a9634ef4981fbf12f306a7575
leeyulgok <dbfrhr20@gmail.com>
2023-12-18 16:07:26 +0900

    docs: 재료 및 요리법 상수화

---
+++ utils/constants/CookingMethodList.ts
@@ -0,0 +1,15 @@
+const COOKING_METHOD = {
+  TITLE: "Cooking Method",
+  LIST: [
+    "Processed foods",
+    "Rice",
+    "Flour",
+    "Vegetables",
+    "Pork",
+    "Chicken meat",
+    "Beef",
+    "Etc",
+  ],
+};
+
+export default COOKING_METHOD;
---
+++ utils/constants/IngredientList.ts
@@ -0,0 +1,6 @@
+const INGREDIENT = {
+  TITLE: "Ingredient",
+  LIST: ["Fried", "Grilling", "Boiling", "Stir-fry", "Seasoned", "Etc"],
+};
+
+export default INGREDIENT;
