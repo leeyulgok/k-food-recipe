591605d886572eb4b64be6e9fb82286f0ae80189
leeyulgok <dbfrhr20@gmail.com>
2023-12-20 18:57:41 +0900

    feat: 검색 기능 추가

--- components/navBar/SearchBar.tsx
+++ components/navBar/SearchBar.tsx
@@ -4,10 +4,19 @@ import { useState } from "react";
 import Image from "next/image";
 import AnimateWidth from "../animate/AnimateWidth";
 import AnimateFade from "../animate/AnimateFade";
+import { useRouter } from 'next/router';
 import styles from "./SearchBar.module.css";
 
 const SearchBar = () => {
+  const [searchQuery, setSearchQuery] = useState("");
   const [isInputVisible, setInputVisible] = useState(false);
+  const router = useRouter();
+
+  const handleSearch = () => {
+    if (searchQuery) {
+      router.push(`/list?search=${encodeURIComponent(searchQuery)}`);
+    }
+  };
 
   return (
     <div className={styles.searchBarContainer}>
@@ -19,12 +28,14 @@ const SearchBar = () => {
           <Image src="/images/search.webp" width={20} height={20} alt="검색" />
         </button>
         <AnimateFade isVisible={isInputVisible} className={styles.searchInputContainer}>
-          <input
+        <input
             type="text"
             className={styles.searchInput}
             placeholder="Search..."
+            value={searchQuery}
+            onChange={(e) => setSearchQuery(e.target.value)}
           />
-          <button className={styles.searchSubmitButton}>
+          <button className={styles.searchSubmitButton} onClick={handleSearch}>
             Search
           </button>
         </AnimateFade>
