b9f69536f3f8acc601d578057506c5406f15d223
leeyulgok <dbfrhr20@gmail.com>
2023-12-26 17:38:15 +0900

    feat: favicon, title 추가

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,20 @@
 import '../app/globals.css'
 import RootLayout from '../app/layout'
 import DataProvider from '../contexts/DataProvider';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }: any) {
   return (
-    <DataProvider>
-      <RootLayout>
-        <Component {...pageProps} />
-      </RootLayout>
-    </DataProvider>
+    <>
+      <Head>
+        <title>How to Make K-Food?</title>
+      </Head>
+      <DataProvider>
+        <RootLayout>
+          <Component {...pageProps} />
+        </RootLayout>
+      </DataProvider>
+    </>
   )
 }
 
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,6 +5,7 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          <link rel="icon" href="/images/k-food.ico" />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link href="https://fonts.googleapis.com/css2?family=Gowun+Dodum&display=swap" rel="stylesheet" />
