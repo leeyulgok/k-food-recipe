190995a59f4f77d6d93d30f237474e350d1193a7
leeyulgok <dbfrhr20@gmail.com>
2023-12-27 18:29:59 +0900

    docs: filePath 경로 설정

--- utils/constants/filePath.ts
+++ utils/constants/filePath.ts
@@ -1,1 +1,3 @@
-export const FILE_PATH = "public/data/TB_RECIPE_SEARCH.csv";
+import path from 'path';
+
+export const FILE_PATH = path.join(process.cwd(), 'public', 'data', 'TB_RECIPE_SEARCH.csv');
