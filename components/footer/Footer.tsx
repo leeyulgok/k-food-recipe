36e6286d9706642bde8b11041f0fa214227fa84d
leeyulgok <dbfrhr20@gmail.com>
2023-12-27 12:04:38 +0900

    refactor: footer 상수화

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -3,25 +3,25 @@ import FooterSection from "./FooterSection";
 import styles from "./Footer.module.css";
 import INGREDIENT from "@/utils/constants/ingredientList";
 import COOKING_METHOD from "@/utils/constants/cookingMethodList";
+import FOOTER_CONTENTS from "@/utils/constants/footerContents";
 
 const Footer = () => {
-  const HMFood = {
-    TITLE: "How to Make K-Food",
-    CONTENTS: `Do you like Korean food?\nNow make it simple at home.\nMake it lightly with a video without difficulty.`,
-  };
-
-  const ProjectDes = {
-    TITLE: "Project Description",
-    CONTENTS: `© ${new Date().getFullYear()} Project. All rights reserved.`,
-  };
-
   return (
     <footer className={styles.footer}>
       <div className={styles.footerContent}>
         <FooterSection title={INGREDIENT.TITLE} contents={INGREDIENT.LIST} />
-        <FooterSection title={COOKING_METHOD.TITLE} contents={COOKING_METHOD.LIST} />
-        <FooterSection title={HMFood.TITLE} contents={HMFood.CONTENTS} />
-        <FooterSection title={ProjectDes.TITLE} contents={ProjectDes.CONTENTS} />
+        <FooterSection
+          title={COOKING_METHOD.TITLE}
+          contents={COOKING_METHOD.LIST}
+        />
+        <FooterSection
+          title={FOOTER_CONTENTS.HM_FOOD.TITLE}
+          contents={FOOTER_CONTENTS.HM_FOOD.CONTENTS}
+        />
+        <FooterSection
+          title={FOOTER_CONTENTS.PROJECT_DES.TITLE}
+          contents={FOOTER_CONTENTS.PROJECT_DES.CONTENTS}
+        />
       </div>
     </footer>
   );
---
+++ utils/constants/footerContents.ts
@@ -0,0 +1,12 @@
+const FOOTER_CONTENTS = {
+  HM_FOOD: {
+    TITLE: "How to Make K-Food",
+    CONTENTS: `Do you like Korean food?\nNow make it simple at home.\nMake it lightly with a video without difficulty.`,
+  },
+  PROJECT_DES: {
+    TITLE: "Project Description",
+    CONTENTS: `© ${new Date().getFullYear()} Project. All rights reserved.`,
+  },
+};
+
+export default FOOTER_CONTENTS;
