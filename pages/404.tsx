56530c9b918b6ad23fa55aa2802b42a1873e82fb
leeyulgok <dbfrhr20@gmail.com>
2023-12-29 17:48:11 +0900

    feat: 404 pages 추가

---
+++ pages/404.tsx
@@ -0,0 +1,11 @@
+import styles from "./404.module.css";
+
+const Custom404 = () => {
+  return (
+    <div className={styles.errorContainer}>
+      <h2>404 - This page could not be found.</h2>
+    </div>
+  );
+};
+
+export default Custom404;
